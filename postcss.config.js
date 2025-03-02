07b67e527c34c51824c7f894183754200b607b35
unknown <kwameroland5@gmail.com>
2025-03-01 19:55:45 -0600

    updated again with tailwind css

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'konta-orange': '#ff6600', // Adjust this hex value to match the site's orange
+        'konta-dark': '#1e2530',   // Dark background color
+      }
+    },
+  },
+  plugins: [],
+}
